925c3e555dba04b78bdc311d81198378b4404f82
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2023-06-11 15:54:37 +0300

    End of section 2

---
+++ -- SQLite.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users(Id, UserName)
+VALUES(5, "Bob");
+
+INSERT INTO Users(Id, UserName)
+VALUES(6, "Tom");
+
+INSERT INTO Users(Id, UserName)
+VALUES(7, "Jane");
---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+app.MapControllers();
+
+app.Run();
