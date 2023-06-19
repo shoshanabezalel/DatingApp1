5ef0554819b1e8ec8fa9a60e130a29816f75083b
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2023-06-19 21:10:52 +0300

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  model: any = {};
+
+  constructor(public accountService: AccountService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => console.log(error)
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+
+}
